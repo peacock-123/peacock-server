2801ce673d9f77a548f76d32be7a425962169b5b
Jake Son <sonjeabin@gmail.com>
2024-07-20 17:52:15 +0900

    feat: redis session 설정
    Co-authored-by: dygma0 <404err@naver.com>

--- peacock-api/build.gradle.kts
+++ peacock-api/build.gradle.kts
@@ -3,6 +3,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.session:spring-session-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
---
+++ peacock-api/src/main/kotlin/com/peacock/api/config/RedisConfig.kt
@@ -0,0 +1,19 @@
+package com.peacock.api.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
+@Configuration(proxyBeanMethods = false)
+@EnableRedisHttpSession(
+    redisNamespace = "\${spring.session.redis.namespace}",
+    maxInactiveIntervalInSeconds = 3600,
+)
+class RedisConfig {
+    @Bean
+    fun springSessionDefaultRedisSerializer(objectMapper: ObjectMapper): RedisSerializer<Any> =
+        GenericJackson2JsonRedisSerializer(objectMapper)
+}
