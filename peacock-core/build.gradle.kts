c71602fea491a2534a717abf6b594ca0b45edebc
Jake Son <sonjeabin@gmail.com>
2024-08-31 17:31:58 +0900

    chore: jooq 설정
    Co-authored-by: dygma0 <404err@naver.com>

--- peacock-core/build.gradle.kts
+++ peacock-core/build.gradle.kts
@@ -1,5 +1,15 @@
+plugins {
+    id("org.jooq.jooq-codegen-gradle") version "3.19.10"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.jooq:jooq:3.19.10")
+    implementation("org.jooq:jooq-meta:3.19.10")
+    implementation("org.jooq:jooq-codegen:3.19.10")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+
+    jooqCodegen("org.postgresql:postgresql")
 
     runtimeOnly("org.postgresql:postgresql")
 }
@@ -10,4 +20,42 @@ tasks.bootJar {
 
 tasks.jar {
     enabled = true
+}
+
+jooq {
+    configuration {
+        jdbc {
+            val dbUrl: String by project
+            val dbUser: String by project
+            val dbPassword: String by project
+
+            driver = "org.postgresql.Driver"
+            url = dbUrl
+            user = dbUser
+            password = dbPassword
+        }
+
+        generator {
+            name = "org.jooq.codegen.KotlinGenerator"
+
+            generate {
+                isDeprecated = false
+                isTables = true
+                isRecords = false
+                isIndexes = false
+                isDefaultCatalog = false
+            }
+
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                includes = ".*"
+                inputSchema = "public"
+            }
+
+            target {
+                packageName = "com.peacock.core.mata"
+                directory = "build/generated-sources/jooq"
+            }
+        }
+    }
 }
