9d52555e606ed4513af2c59c63013d2de8bda6ea
Jake Son <sonjeabin@gmail.com>
2024-09-22 16:21:37 +0900

    chore: swagger server 재수정
    Co-authored-by: dygma0 <404err@naver.com>

--- peacock-api/src/main/kotlin/com/peacock/ApiApplication.kt
+++ peacock-api/src/main/kotlin/com/peacock/ApiApplication.kt
@@ -1,9 +1,17 @@
 package com.peacock
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 
+@OpenAPIDefinition(
+    servers = [
+        Server(url = "https://api.peacock.vm.cd80.run", description = "prod"),
+        Server(url = "http://localhost:8080", description = "local"),
+    ],
+)
 @SpringBootApplication
 @EnableJdbcAuditing
 class ApiApplication
